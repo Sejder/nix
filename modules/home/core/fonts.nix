7949c032f429f0e2d0ed90e099cf1b5631cf7abe
Mikkelsej <mikkel.sejdelin@gmail.com>
2025-08-20 13:00:06 +0200

    Added fonts

---
+++ modules/home/core/fonts.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, lib, ... }:
+
+{
+  options.features.fonts = lib.mkOption {
+    type = lib.types.str;
+    default = "jetbrainsMono";
+    description = "Select which font to enable by default.";
+  };
+
+  config = lib.mkIf (config.features.fonts == "jetbrainsMono") {
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "JetBrains Mono" ];
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+      };
+    };
+
+    home.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+    ];
+  };
+}
