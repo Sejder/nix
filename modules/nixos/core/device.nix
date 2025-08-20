c1e74a838c488e394e43d986c78085c7da40a152
Mikkelsej <mikkel.sejdelin@gmail.com>
2025-08-20 14:00:01 +0200

    updated power-management

--- modules/nixos/core/device.nix
+++ modules/nixos/core/device.nix
@@ -1,49 +1,48 @@
 { pkgs, lib, config, ... }:
-
-let
+let 
   cfg = config.device;
 in
-
 {
-  options = {
-    device = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = "Specify the type of device: laptop, desktop, or server.";
-    };
+  options.device = lib.mkOption {
+    type = lib.types.str;
+    default = "";
+    description = "Select device type";
   };
-
+  
+  
   config = {
-    assertions =
-      [ { assertion = config.device != null;
-          message = "Device type must be set";
-        }
+    assertions = [
+      {
+        assertion = cfg != "";
+        message = "Device type must be set at the host level (device = ...).";
+      }
+    ];
+
+    services.tlp = {
+      enable = true;
+
+      settings = lib.mkMerge [
+        (lib.mkIf (cfg == "laptop") {
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+            WIFI_PWR_ON_AC = "off";
+            WIFI_PWR_ON_BAT = "on";
+        })
+
+        (lib.mkIf (cfg == "desktop") {
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        })
+
+        (lib.mkIf (cfg == "server") {
+            CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+            CPU_BOOST_ON_AC = 0;
+        })
       ];
-    
-    services.tlp = lib.mkIf (config.device == "laptop") {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        WIFI_PWR_ON_AC = "off";
-        WIFI_PWR_ON_BAT = "on";
-      };
-    } // lib.mkIf (config.device == "desktop") {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      };
-    } // lib.mkIf (config.device == "server") {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-        CPU_BOOST_ON_AC = 0;
-      };
     };
   };
 }
