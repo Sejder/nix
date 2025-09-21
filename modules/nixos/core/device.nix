29499ced3260f45d98fd0424a5780be61efd940e
Mikkelsej <mikkel.sejdelin@gmail.com>
2025-09-21 11:08:02 +0200

    Small changes

--- modules/nixos/core/device.nix
+++ modules/nixos/core/device.nix
@@ -49,10 +49,7 @@ in {
       
       (lib.mkIf (cfg == "server") {
         tlp.settings = {
-          CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-          CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-          CPU_BOOST_ON_AC = 0;
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
         };
       })
     ];
