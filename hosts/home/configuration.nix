316cda70f98cf91ff032031b5df25cefb4dcadfc
Mikkelsej <mikkel.sejdelin@gmail.com>
2025-10-05 16:55:51 +0200

    Small changes

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -24,10 +24,10 @@
     };
   };
 
-  fileSystems."/data" =
-    { device = "/dev/disk/by-uuid/e3c53c2c-8375-4ad3-bd2a-daf94a419318";
-      fsType = "ext4";
-    };
-    
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/150C-2773";
+    fsType = "ext4";
+  };
+
   system.stateVersion = "25.05";
 }
