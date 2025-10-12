ee4c13619b866dca37a07f09d6d73185f03d56b6
Mikkelsej <mikkel.sejdelin@gmail.com>
2025-10-12 21:23:08 +0200

    Small changes

---
+++ modules/nixos/optional/server/nas/netgear.nix
@@ -0,0 +1,31 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.features.server.nas.netgear;
+in
+{
+  options.features.server.nas.netgear = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable netgear nas";
+    };
+  };
+  
+  config = lib.mkIf cfg.enable {
+    services.nginx.virtualHosts = {
+      "netgear.${config.networking.hostName}" = {
+          locations."/" = {
+            proxyPass = "http://192.168.87.165:80";
+            proxyWebsockets = true;
+            recommendedProxySettings = true;
+            extraConfig = ''
+              client_max_body_size 100G;           
+              proxy_read_timeout 600s;
+              proxy_send_timeout 600s;
+            '';
+        };
+      };
+    };
+  };
+}
