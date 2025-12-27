876dac2f3a1858ca8e95c61afbe44ef3cf3bb179
Sejder <mikkel.sejdelin@gmail.com>
2025-12-27 12:48:43 +0100

    Small changes

--- modules/home/optional/editors/zed.nix
+++ modules/home/optional/editors/zed.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.features.editors.zed;
+  langCfg = config.features.programmingLanguages;
 in
 {
   options.features.editors.zed.enable = lib.mkEnableOption "Enable zed as editor";
@@ -18,10 +19,77 @@ in
     programs.zed-editor = {
       enable = true;
       extensions = [
-        "nix"
         "git-firefly"
-      ];
+      ]
+      ++ lib.optional langCfg.nix.enable "nix"
+      ++ lib.optional langCfg.c-sharp.enable "C#"
+      ++ lib.optional langCfg.R.enable "R"
+      ++ lib.optional langCfg.java.enable "java"
+      ++ lib.optional langCfg.python.enable "python"
+      ++ lib.optional langCfg.rust.enable "rust"
+      ++ lib.optional config.features.documentWriters.typst.enable "typst";
       package = unstable-pkgs.zed-editor;
+      userSettings = {
+        agent = {
+          inline_assistant_model = {
+            provider = "copilot_chat";
+            model = "gpt-5-mini";
+          };
+          model_parameters = [];
+        };
+        git = {
+          inline_blame = {
+            enabled = false;
+          };
+        };
+        show_edit_predictions = false;
+        inlay_hints = {
+          show_background = true;
+          enabled = true;
+        };
+        toolbar = {
+          code_actions = false;
+        };
+        minimap = {
+          thumb = "always";
+          show = "never";
+        };
+        autosave = {
+          after_delay = {
+            milliseconds = 1000;
+          };
+        };
+        features = {
+          edit_prediction_provider = "copilot";
+        };
+        theme = {
+          mode = "system";
+          light = "One Light";
+          dark = "One Dark";
+        };
+        base_keymap = "JetBrains";
+        lsp = lib.mkMerge [
+          (lib.mkIf config.features.documentWriters.typst.enable {
+            tinymist = {
+              settings = {
+                exportPdf = "onSave";
+                outputPath = "$root/$name";
+              };
+            };
+          })
+          (lib.mkIf langCfg.nix.enable {
+            nil = {
+              settings = {
+                nix = {
+                  flake = {
+                    autoArchive = true;
+                  };
+                };
+              };
+            };
+          })
+        ];
+      };
     };
   };
 }
